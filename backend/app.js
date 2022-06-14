34b233e49ffb65e23ba749682a0db9c28b3e5a91
Andrei Balzan <andreibalzan@gmail.com>
2022-06-14 08:52:48 +0100

    creating Post model, connecting to the database

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 app.use(bodyParser.json());
+
+const MONGODB_URI = 'mongodb+srv://andrei:R9LeoQS78jgS0g04@nodejscompletecourseclu.xgbdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -15,4 +18,10 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect(MONGODB_URI)
+    .then(result => {
+        console.log('Connected to MongoDB');
+        app.listen(8080);
+    }).catch(err => {
+        console.log('Error connecting to MongoDB:', err.message);
+    })
