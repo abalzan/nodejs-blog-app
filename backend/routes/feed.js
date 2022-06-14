1de4d49cdcb47fb4f18fd7c3026fc19c8e2bb494
Andrei Balzan <andreibalzan@gmail.com>
2022-06-14 07:33:00 +0100

    adding validation

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const feedController = require('../controllers/feed');
+const {body} = require('express-validator');
 
 router.get('/posts', feedController.getPosts);
-router.post('/posts', feedController.createPost);
+router.post('/posts',
+    [
+        body('title').trim().isLength({min: 5}),
+        body('content').trim().isLength({min: 5})
+    ],
+    feedController.createPost);
 
 module.exports = router;
