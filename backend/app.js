e0b9a7b91cfd85e254e8825271bf57ad78056718
Andrei Balzan <andreibalzan@gmail.com>
2022-06-19 15:11:59 +0100

    create user model

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 const path = require("path");
 const multer = require('multer');
 
@@ -39,6 +40,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
