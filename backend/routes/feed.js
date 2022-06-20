2200d115d188bafcc69bfa4d7721dfdf9f236a5a
Andrei Balzan <andreibalzan@gmail.com>
2022-06-20 16:45:19 +0100

    add authorisations checks

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -13,7 +13,7 @@ router.post('/posts', isAuth,
     feedController.createPost);
 
 router.get('/posts/:postId', isAuth, feedController.getPostById)
-router.put('/posts/:postId', [
+router.put('/posts/:postId', isAuth, [
     body('title').trim().isLength({min: 5}),
     body('content').trim().isLength({min: 5})
 ],feedController.updatePost)
