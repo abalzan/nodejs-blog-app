c27e3af29679f097a7889ce8322af95cf815e585
Andrei Balzan <andreibalzan@gmail.com>
2022-07-13 08:44:32 +0100

    handling errors graphql

--- backend/app.js
+++ backend/app.js
@@ -44,6 +44,19 @@ app.use('/graphql', graphqlHTTP({
         schema: graphqlSchema,
         rootValue: graphqlResolver,
         graphiql: true,
+        formatError(err) {
+            if (!err.originalError) {
+                return err;
+            }
+            const data = err.originalError.data;
+            const message = err.message || 'An error occurred.';
+            const code = err.originalError.statusCode || 500;
+            return {
+                message: message,
+                status: code,
+                data: data
+            }
+        }
     })
 );
 
